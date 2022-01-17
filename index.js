5ba7c7e92ed78b75755724410072fe26c61dd313
User <user@Users-MacBook-Pro.local>
2022-01-17 15:45:25 -0600

    clean up naming

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServer } = require('apollo-server');
 const { GraphQLObjectType, GraphQLSchema } = require('graphql/type');
 const { GraphQLList, GraphQLNonNull } = require('graphql/type/definition');
 const {
@@ -7,115 +7,120 @@ const {
 } = require('graphql/type/scalars');
 const { ApolloServerPluginLandingPageGraphQLPlayground } = require('apollo-server-core');
   
-const TodoType = new GraphQLObjectType({
-    name: 'Todo',
+const ToDoType = new GraphQLObjectType({
+    name: 'ToDo',
     description: 'A task that needs to be done',
     fields: () => ({
-      id: { type: new GraphQLNonNull(GraphQLInt) },
-      title: { type: new GraphQLNonNull(GraphQLString) },
-      description: { type: new GraphQLNonNull(GraphQLString) },
+        id: { type: new GraphQLNonNull(GraphQLInt) },
+        title: { type: new GraphQLNonNull(GraphQLString) },
+        description: { type: new GraphQLNonNull(GraphQLString) },
     }),
-  });
+});
   
-  const RootQueryType = new GraphQLObjectType({
+const RootQueryType = new GraphQLObjectType({
     name: 'Query',
     description: 'Root Query',
     fields: () => ({
-      todos: {
-        type: new GraphQLList(TodoType),
-        description: 'List of all todos',
-        resolve: () => todos,
-      },
+        toDos: {
+            type: new GraphQLList(ToDoType),
+            description: 'List of all to-dos',
+            resolve: () => toDos,
+        },
     }),
-  });
+});
 
-  const IdType = new GraphQLObjectType({
+const IdType = new GraphQLObjectType({
     name: 'Id',
     description: 'An Id returned by the server',
     fields: () => ({
-      id: { type: GraphQLInt },
+        id: { type: GraphQLInt },
     }),
   });
   
-  const RootMutationType = new GraphQLObjectType({
+const RootMutationType = new GraphQLObjectType({
     name: 'Mutation',
     description: 'Root Mutation',
     fields: () => ({
-      addTodo: {
-        type: TodoType,
-        description: 'Adds a todo',
-        args: {
-          title: { type: new GraphQLNonNull(GraphQLString) },
-          description: { type: new GraphQLNonNull(GraphQLString) },
+        addToDo: {
+            type: ToDoType,
+            description: 'Adds a to-do',
+            args: {
+                title: { type: new GraphQLNonNull(GraphQLString) },
+                description: { type: new GraphQLNonNull(GraphQLString) },
+            },
+            resolve: (parent, args) => {
+                const ids = toDos.map(o => {
+                    return o.id;
+                });
+                const max = Math.max(...ids);
+                const toDo = {
+                    id: max + 1,
+                    title: args.title,
+                    description: args.description,
+                };
+                toDos.push(toDo);
+                return toDo;
+            },
         },
-        resolve: (parent, args) => {
-            const ids = todos.map(o => {
-                return o.id;
-              });
-              const max = Math.max(...ids);
-          const todo = {
-            id: max + 1,
-            title: args.title,
-            description: args.description,
-          };
-          todos.push(todo);
-          return todo;
+        deleteToDo: {
+            type: IdType,
+            description: 'Deletes a toDo',
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLInt) },
+            },
+            resolve: (parent, args) => {
+                const index = toDos.findIndex(function(o) {
+                    return o.id === args.id;
+                })
+                if (index !== -1) toDos.splice(index, 1);
+                return { id: args.id};
+            },
         },
-      },
-      deleteTodo: {
-        type: IdType,
-        description: 'Deletes a todo',
-        args: {
-          id: { type: new GraphQLNonNull(GraphQLInt) },
-        },
-        resolve: (parent, args) => {
-            const index = todos.findIndex(function(o){
-                return o.id === args.id;
-           })
-           if (index !== -1) todos.splice(index, 1);
-          return { id: args.id};
-        },
-      },
     }),
-  });
+});
   
-  const schema = new GraphQLSchema({
+const schema = new GraphQLSchema({
     query: RootQueryType,
     mutation: RootMutationType,
-  });
+});
 
-const todos = [
+const toDos = [
     {
-      id: 1,
-      title: 'Wash dishes',
-      description: 'Get them clean.',
+        id: 1,
+        title: 'Laundry',
+        description: 'Whites and colors',
     },
     {
-      id: 2,
-      title: 'Do laundry',
-      description: 'Remember to fold',
+        id: 2,
+        title: 'Dry cleaner',
+        description: 'Pick up suit',
     },
     {
-      id: 3,
-      title: 'Clean bathroom',
-      description: 'Get it clean.',
+        id: 3,
+        title: 'Clean bathroom',
+        description: 'Need to get cleaning supplies',
     },
     {
-      id: 4,
-      title: 'Comb hair',
-      description: 'Get them straight.',
+        id: 4,
+        title: 'Homework',
+        description: 'Physics assignment',
     },
-  ];
-  const resolvers = {
+    {
+        id: 5,
+        title: 'Groceries',
+        description: 'Eggs, milk, bacon, cheese',
+    },
+];
+const resolvers = {
     Query: {
-      todos: () => todos,
+        toDos: () => toDos,
     },
-  };
+};
 
-const server = new ApolloServer({ schema, resolvers,   plugins: [
+const server = new ApolloServer({ schema, resolvers, plugins: [
     ApolloServerPluginLandingPageGraphQLPlayground(),
-  ], });
+], });
 
 server.listen({ port: process.env.PORT || 4000 }).then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+    console.log(`🚀  Server ready at ${url}`);
 });
